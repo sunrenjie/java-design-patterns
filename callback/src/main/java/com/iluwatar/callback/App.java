6fd7a9448b47364f8c2b802a65b9f4182c977560
Renjie Sun <renjiesun.MacOSX-10.9@gmail.com>
2016-01-06 10:38:04 +0800

    [callback] Change callback to lambda expression

--- callback/src/main/java/com/iluwatar/callback/App.java
+++ callback/src/main/java/com/iluwatar/callback/App.java
@@ -14,12 +14,7 @@ public class App {
    */
   public static void main(String[] args) {
     Task task = new SimpleTask();
-    Callback callback = new Callback() {
-      @Override
-      public void call() {
-        System.out.println("I'm done now.");
-      }
-    };
+    Callback callback = () -> System.out.println("I'm done now.");
     task.executeWith(callback);
   }
 }
